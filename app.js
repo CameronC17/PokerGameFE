841f6d10193a420c28c5046a3cc4b1ce18fc5215
Laura Salivonaite <laura.salivonaite@skybettingandgaming.com>
2016-09-26 11:00:50 +0100

    connected css to homepage

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ var ejs= require('ejs');
 var layout= require('express-ejs-layouts');
 var sroutes= require('./config/static-routes')
 
+
+
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/public/css'));
+app.use(layout);
 app.use(sroutes);
+
 
 app.set('port', process.env.PORT || 8085);
 var server = app.listen(app.get('port'), function() {
