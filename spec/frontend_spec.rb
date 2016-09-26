dc568f0a21ead3ce63e9e86835083c95dc76f0ee
alexmacaulay <alex@macaulay.me.uk>
2016-09-26 11:21:37 +0100

    basic setup of rspec

---
+++ spec/frontend_spec.rb
@@ -0,0 +1,15 @@
+describe "Poker game front end" do 
+
+  before:all do 
+    @driver = Selenium::WebDriver.for :chrome
+  end
+
+  it "should provide users a way to register an account" do 
+
+  end
+
+  it "should provide users a way to login" do 
+
+  end
+
+end
