5a91f4c940ab5c9cc9e1efa60b224a938c08ea89
CameronC17 <cameronchalmers@hotmail.co.uk>
2016-09-26 12:02:15 +0100

    reformatted to normal html files

--- config/static-routes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var statics = require('../controllers/statics.js');
-
-//Homepage / Create
-router.route('/')
-    .get(statics.index);
-
-module.exports = router;
