9013001180d17823f17d6278a11804090995484f
alexmacaulay <alex@macaulay.me.uk>
2016-09-27 13:23:59 +0100

    added some messages for current fails

--- spec/frontend_spec.rb
+++ spec/frontend_spec.rb
@@ -2,7 +2,7 @@ describe "Poker game front end" do
 
   before:all do 
     @driver = Selenium::WebDriver.for :chrome
-    @url = "http://127.0.0.1:8080/"
+    @url = "localhost:3000"
     @username = "test1"
     @password = "password"
     @wallet = "100"
@@ -13,18 +13,19 @@ describe "Poker game front end" do
   end
 
   it 'should have a home page that shows links to register, login and the game' do 
-    register = @driver.find_element(id: "homepage").attribute("outerHTML")
-    expect(register).to include "<a", "href", "Home"
-    register = @driver.find_element(id: "registerpage").attribute("outerHTML")
-    expect(register).to include "<a", "href", "Register"
+    home_link = @driver.find_element(id: "homepage").attribute("outerHTML")
+    expect(home_link).to include "<a", "Home"
+    register_link = @driver.find_element(id: "registerpage").attribute("outerHTML")
+    expect(register_link).to include "<a", "Register"
     login_link = @driver.find_element(id: "loginpage").attribute("outerHTML")
-    expect(login_link).to include "a", "href", "Login"
+    expect(login_link).to include "a", "Login"
     game_link = @driver.find_element(id: "gamepage").attribute("outerHTML")
-    expect(game_link).to include "a", "href", "Game"
+    expect(game_link).to include "a", "Game"
   end
 
-  it "should provide users a way to register an account" do 
+  it 'should provide users a way to register an account' do 
     @driver.find_element(id: "registerpage").click
+    puts "\nThis will fail due to the title not changing"
     expect(@driver.title).to include "Register"
     @driver.find_element(class: "username").send_keys @username
     @driver.find_element(class: "password").send_keys @password
@@ -34,8 +35,9 @@ describe "Poker game front end" do
     # expect(@driver.title).to include "Login"
   end
 
-  it "should provide users a way to login" do
+  it 'should provide users a way to login' do
     @driver.find_element(id: "loginpage").click
+    puts "\nThis will fail due to the title not changing"
     expect(@driver.title).to include "Login"
     @driver.find_element(class: "username").send_keys @username
     @driver.find_element(class: "password").send_keys @password
@@ -43,6 +45,17 @@ describe "Poker game front end" do
     submit[-1].click
     # This is commented out as it is currently failing and will do until redirect pages work correctly and have titles
     # expect(@driver.title).to include "Game"
+  end
+
+  it 'should not allow an unauthorised user to visit the game page' do 
+    @driver.find_element(id: "gamepage").click
+     puts "\nThis will fail atm due to no authorisation being included"
+    expect(@driver.title).to include "Login"
+    expect(@driver.page_source).to include "You must login first before joining a game"
+  end
+
+  it 'should allow the user to select different stakes' do 
+
   end
 
 end
