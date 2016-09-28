dd73aea7789d2b7112f8651a22e0347aef7fc59c
alexmacaulay <alex@macaulay.me.uk>
2016-09-28 17:00:11 +0100

    checking cards on table and refactor

--- spec/frontend_spec.rb
+++ spec/frontend_spec.rb
@@ -90,9 +90,27 @@ describe "Poker game front end" do
   end
 
   it 'should allow a game to be started and the first cards to be dealt' do
+    gametype = ["single", "multiplayer"]
+    player
+    2.times do |i|
+      @driver.find_element(id: "gamepage").click
+      @driver.find_element(id: gametype[i]).click
+      @driver.find_element(id: "start").click
+      cards = @driver.find_elements(css: "#top-left div")
+      expect(cards.length).to eq 2
+      cards = @driver.find_elements(css: "#top-right div")
+      expect(cards.length).to eq 2
+      cards = @driver.find_elements(css: "#bottom-left div")
+      expect(cards.length).to eq 2
+      cards = @driver.find_elements(css: "#bottom-middle div")
+      expect(cards.length).to eq 2
+      cards = @driver.find_elements(css: "#bottom-right div")
+      expect(cards.length).to eq 2
+      @driver.get(@url)
+    end
     # click start button
-    # check the flop has occurred
     # check each player has 2 cards
+    # check the flop has occurred
   end
 
 end
