b909a226e200f1fbd8e213499a7a75aae6293ae0
alexmacaulay <alex@macaulay.me.uk>
2016-09-29 10:48:24 +0100

    split single file into two files, accounts_spec and game_spec that cover their relative things

--- spec/accounts_spec.rb
+++ spec/accounts_spec.rb
@@ -5,7 +5,6 @@ describe "Poker game front end" do
     @url = "localhost:3002"
     @username = "testz"
     @password = "password"
-    @wallet = 100
     @stake = "1000"
     @invalid_username = "invalid invalid invalid"
     @invalid_password = "thisisawrongpassword"
@@ -22,7 +21,7 @@ describe "Poker game front end" do
     expect(@driver.find_element(id: "gamepage").attribute("outerHTML")).to include "a", "Game"
   end
 
-  # current causes the app to crash
+  # currently causes the app to crash
   # it 'should provide users a way to register an account' do
   #   @driver.find_element(id: "registerpage").click
   #   expect(@driver.title).to include "Register"
@@ -33,7 +32,7 @@ describe "Poker game front end" do
   #   expect(@driver.find_element(id: "register-msg").attribute("innerHTML")).to include "You have successfully registered. Now you can login"
   # end
 
-  # current causes the app to crash
+  # currently causes the app to crash
   # it 'should not allow users to register an account using invalid details' do
   #   @driver.find_element(id: "registerpage").click
   #   expect(@driver.title).to include "Register"
@@ -65,45 +64,18 @@ describe "Poker game front end" do
     expect(@driver.find_element(id: "login-msg").attribute("innerHTML")).to include "A user does not exist with the given details"
   end
 
+  it 'should allow the user to logout' do
+
+  end
+
   it 'should not allow an unauthorised user to start a game' do
     @driver.find_element(id: "gamepage").click
     # currently fails as i do not have alert popping on my local copy
     expect(@driver.page_source).to include "You have to login in order to play."
   end
 
-  it 'should allow the user to select different stakes for single player and multiplayer games' do
-    # Once authorisation is working I will need a login method made and linked here
-    gametype = ["single", "multiplayer"]
-    2.times do |i|
-      @driver.find_element(id: "gamepage").click
-      @driver.find_element(id: gametype[i]).click
-      @driver.find_element(id: "option1").click
-      expect(@driver.find_element(id: "stake").attribute("value")).to include "100"
-      @driver.find_element(id: "option2").click
-      expect(@driver.find_element(id: "stake").attribute("value")).to include "50"
-      @driver.find_element(id: "option3").click
-      expect(@driver.find_element(id: "stake").attribute("value")).to include "20"
-      @driver.find_element(id: "stake").clear
-      @driver.find_element(id: "stake").send_keys @stake
-      expect(@driver.find_element(id: "stake").attribute("value")).to include @stake
-      @driver.get(@url)
-    end
-  end
-
-  it 'should allow a game to be started and the first cards to be dealt (flop and player cards)' do
-    login
-    gametype = ["single", "multiplayer"]
-    player_position = ["top-left", "top-right", "bottom-left", "bottom-middle", "bottom-right"]
-    2.times do |game|
-      @driver.find_element(id: "gamepage").click
-      @driver.find_element(id: gametype[game]).click
-      @driver.find_element(id: "start").click
-      5.times do |player|
-        expect(@driver.find_elements(css: "##{player_position[player]} .card").length).to eq 2
-      end
-      @driver.get(@url)
-    end
-    # check the flop has occurred
+  after:all do
+    @driver.quit
   end
 
 end
