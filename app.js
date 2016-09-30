d652579a5a510ee71549ce6e869abcd929ed5238
CameronC17 <cameronchalmers@hotmail.co.uk>
2016-09-30 09:06:38 +0100

    changed port number for devops

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3002;
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
